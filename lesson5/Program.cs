9a6f60af7a5075267adbc00c9b5b07292ac03685
Vadim Pudov <pvb@rambler.ru>
2022-08-05 19:45:35 +0700

    user greeting

---
+++ lesson5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Вадим")
+{
+    Console.WriteLine("Здоров, Друган!");
+}
+else { Console.WriteLine("привет, $username"); }
